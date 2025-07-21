8b2786def4c95dbcab83ab88db2ef8e3d7965c42
jameelf22labs <jameel.m@f22labs.com>
2025-07-21 13:03:53 +0530

    setup nest js app

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory : (config : ConfigService) => ({
+        dialect : 'postgres',
+        host : config.get<string>('POSTGRESS_HOST'),
+        port : config.get<number>('POSTGRESS_PORT'),
+        username : config.get<string>('POSTGRESS_USERNAME'),
+        password : config.get<string>('POSTGRESS_PASSWORD'),
+        database : config.get<string>('POSTGRESS_DB'),
+        autoLoadModels: true, 
+        logging : true
+      })
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
