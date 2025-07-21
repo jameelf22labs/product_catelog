7466a80fcfda9669c81eee873fcb64c65ac6aa2e
jameelf22labs <jameel.m@f22labs.com>
2025-07-21 19:41:20 +0530

    get ip issue

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Product Filter')
     .setVersion('1.0')
-    .setDescription('Prodcut Catelog filter')
+    .setDescription('Product Catalog filter')
     .build();
+
+  app.set('trust proxy', 1);
 
   const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, swaggerDoc);
